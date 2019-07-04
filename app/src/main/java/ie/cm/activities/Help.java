b838764de21179a23922aad8582257f742d4e328
Vivek <Viv3kdev@gmail.com>
2019-07-04 13:27:22 -0400

    Initial commit

---
+++ app/src/main/java/ie/cm/activities/Help.java
@@ -0,0 +1,14 @@
+package ie.cm.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ie.cm.R;
+
+public class Help extends Base {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.help);
+    }
+}
